e0b7eaa5544465a950333002168727c68102124b
akash2421999 <45793064+akash2421999@users.noreply.github.com>
2021-10-12 16:45:07 +0530

    Initial commit

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path
+from accounts.views import *
+from . import views
+
+urlpatterns = [
+    path("register",views.register,name="register"),
+    path("login",views.login,name="login"),
+    path("logout",views.logout,name="logout"),
+    ]
+
